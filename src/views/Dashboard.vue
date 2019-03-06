6894d2a0d0e1e578ef1cf860085b0c77f7ef5012
dingjinyang <djy.dingjinyang@gmail.com>
2019-03-06 22:10:39 +0800

    修改首页布局

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,15 +2,14 @@
   <v-app id="inspire">
     <v-content>
       <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
+        <v-layout>
+          <v-flex>
             <v-layout justify-center align-center>
               <h1>科研业绩量化系统，努力开发中...</h1>
               <v-tooltip right>
                 <v-btn icon large target="_blank" slot="activator">
                   <v-icon large>code</v-icon>
                 </v-btn>
-                <span>努力开发中...</span>
               </v-tooltip>
             </v-layout>
           </v-flex>
